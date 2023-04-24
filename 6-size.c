9d69be133cc94025d0f85e0daecaf9669dd4a35c
Nglyase <ngozokoye@gmail.com>
2023-04-24 16:01:01 +0000

    update

---
+++ 6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int intsize;
+float floatsize;
+double doublesize;
+char charsize;
+
+{
+	printf("Size of int: %zu bytes\n", sizeof(intsize));
+	printf("Size of float: %zu bytes\n", sizeof(floatsize));
+	return (0);
+}
---
+++ pointers/text.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *Return: 0 on success
+ */
+
+int main(void)
+{
+	int num = 10;
+
+	printf("value of num is = %d\n", num);
+	printf("Address of num is = %p\n", &num);
+	printf("Address of num in hexadecimal is = %p\n", &num);
+	return (0);
+}
